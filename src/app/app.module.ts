60eca282cc29fc40a7a14267e7323c846104ecf6
chiranga wickramasinghe <chirangaw186@gmail.com>
2019-10-05 21:35:30 +0530

    New fixed top nav bar added. New carousel added. Angular MDBootstrap installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomecardsComponent } from './homecards/homecards.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomecardsComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
